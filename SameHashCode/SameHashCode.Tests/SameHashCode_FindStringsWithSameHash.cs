2654bfef5f9a292aa34e9b65c48dc27b18245a50
Serge Shpak <sshpak92@gmail.com>
2022-03-23 15:52:03 +0100

    feat: define the structure of the project and add a relevant test

---
+++ SameHashCode/SameHashCode.Tests/SameHashCode_FindStringsWithSameHash.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace SameHashCode.Tests;
+
+public class SameHashCode_Tests
+{
+    [Fact]
+    public void Seracher_FindStringsWithSameHashCode_ReturnsCorrectObject()
+    {
+        var result = SameHashCode.Searcher.FindStringsWithSameHash();
+        Assert.True(
+            (result.First.GetHashCode() == result.Second.GetHashCode())
+                && (result.Second.GetHashCode() == result.Third.GetHashCode())
+                && (!String.Equals(result.First, result.Second))
+                && (!String.Equals(result.Second, result.Third))
+                && (!String.Equals(result.Third, result.First))
+        );
+    }
+}
---
+++ SameHashCode/SameHashCode/Searcher.cs
@@ -0,0 +1,18 @@
+﻿namespace SameHashCode;
+
+using System;
+
+public struct StringsWithSameHash
+{
+    public string First;
+    public string Second;
+    public string Third;
+}
+
+public static class Searcher
+{
+    public static StringsWithSameHash FindStringsWithSameHash()
+    {
+        throw new NotImplementedException("NYI");
+    }
+}
